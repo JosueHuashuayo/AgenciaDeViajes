29c9f666359b0a0d5b94b9de4101016ab35f9e55
JosueHuashuayo <huashuayojosue@gmail.com>
2022-06-17 01:34:30 -0500

    Modelo Boleta creado con exito

--- Boleta/models.py
+++ Boleta/models.py
@@ -1,3 +1,25 @@
 from django.db import models
-
+from Usuarios.models import Usuarios
+from Pasajes.models import Pasajes
 # Create your models here.
+class Boleta (models.Model):
+    user = models.ForeignKey(
+        Usuarios,
+        null = True,
+        on_delete = models.CASCADE,
+        verbose_name = "Usuario"
+    )
+    ticket = models.ForeignKey(
+        Pasajes,
+        null = True,
+        on_delete = models.CASCADE,
+        verbose_name = "Pasaje"
+    ) 
+    created = models.DateTimeField(
+        auto_now_add = "True",
+        verbose_name = "Fecha de Creacion"
+    )
+    updated = models.DateTimeField(
+        auto_now = "True",
+        verbose_name = "Fecha de Actualizacion"
+    )
